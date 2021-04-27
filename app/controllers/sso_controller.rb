a5e27471db90ae21c5768e51973e903cdbe7c617
Francesco <francesco@unimore.it>
2021-04-27 15:30:30 +0200

    rush into production-like

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -6,5 +6,9 @@ class SsoController < ApplicationController
     session[:cn] = ENV["cn"]
     session[:shib_session] = ENV["Shib-Session-ID"]
     redirect_to :controller => 'main', :action => 'welcome'
+    logger.info session
+    logger.info ENV.inspect
+    logger.info ENV["Shib-Session-ID"]
+    logger.info session[:shib_session]
   end
 end
