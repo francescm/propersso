22a3e6e318fa92751ee1478d9adb034eb92d568d
Francesco <francesco@unimore.it>
2021-04-28 19:41:03 +0200

    proper env names

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -1,14 +1,14 @@
 class SsoController < ApplicationController
   def auth
-    ENV["uid"] = "tizio"
-    ENV["cn"] = "Tizio"
-    session[:uid] = ENV["uid"]
-    session[:cn] = ENV["cn"]
-    session[:shib_session] = ENV["Shib-Session-ID"]
-    redirect_to :controller => 'main', :action => 'welcome'
+    session[:uid] = request.env["uid"]
+    session[:cn] = request.env["cn"]
+    session[:shib_session] = request.env["Shib-Session-ID"]
+
     logger.info session
-    logger.info ENV.inspect
-    logger.info ENV["Shib-Session-ID"]
+    logger.info request.env.inspect
+    logger.info request.env["Shib-Session-ID"]
     logger.info session[:shib_session]
+
+    redirect_to :controller => 'main', :action => 'welcome'
   end
 end
