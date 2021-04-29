d57074b2da5a21d31d9bc6203eeebca360a96719
Francesco <francesco@unimore.it>
2021-04-29 14:38:15 +0200

    params without @

--- app/controllers/slo_controller.rb
+++ app/controllers/slo_controller.rb
@@ -1,13 +1,13 @@
 class SloController < ApplicationController
   def front
-    logger.info("params: #{@params}")
+    logger.info("params: #{params}")
     logger.info("shib session: #{session[:shib_session]}")
     logger.info("session id: #{session.id}")
     session.destroy
-    redirect_to(@params["return"])
+    redirect_to(params["return"])
   end
 
   def back
-    logger.info("params: #{@params}")
+    logger.info("params: #{params}")
   end
 end
