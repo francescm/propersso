50d35b9718f72517cb91afa981c7a0656b30cb97
Francesco <francesco@unimore.it>
2021-04-29 15:42:49 +0200

    add canned response

--- app/controllers/slo_controller.rb
+++ app/controllers/slo_controller.rb
@@ -1,4 +1,6 @@
 class SloController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def front
     logger.info("params: #{params}")
     logger.info("shib session: #{session[:shib_session]}")
@@ -9,5 +11,16 @@ class SloController < ApplicationController
 
   def back
     logger.info("params: #{params}")
+    soap = Nokogiri::XML::Builder.new { |xml|
+      xml.Envelope('xmlns:soap-env' => 'http://schemas.xmlsoap.org/soap/envelope/', 'xmlns:notify' => 'urn:mace:shibboleth:2.0:sp:notify') do
+        xml['soap-env'].Body do
+          xml['soap-env'].LogoutNotificationResponse do
+            xml['notify'].OK
+          end
+        end
+      end
+    }
+    render xml: soap
+
   end
 end
