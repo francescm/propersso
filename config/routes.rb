ab0e8561fe65161505f1a22eee0e6c16db10e267
Francesco <francesco@unimore.it>
2021-04-30 18:46:38 +0200

    add default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'slo/front', as: :slo_front
-  post 'slo/back', as: :slo_back
+  get 'slo/front'
+  post 'slo/back'
   get 'sso/auth', as: :sso_auth
   get 'main/welcome'
+  root to: "main#welcome"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
