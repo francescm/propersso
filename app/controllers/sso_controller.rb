d093c262215bf2773b2f96d75dbb2863922925dc
Francesco <francesco@unimore.it>
2021-04-30 12:50:52 +0200

    discover private_id

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -8,19 +8,15 @@ class SsoController < ApplicationController
     logger.info("Shib-Session-ID as session is #{shib_session}")
 
     # session.id is cleartext
-    # session.session_id is crypted
     logger.info("session id: #{session.id}")
-    logger.info("session_id: #{session['session_id']}")
-    logger.info("serialize: #{::Base64.encode64(Marshal.dump(session.id))}")
-    public_session_id = cookies['_session_id']
-    private_id = Rack::Session::SessionId.new(public_session_id).private_id
-    logger.info("public_session_id: #{public_session_id}")
+
+    private_id = Rack::Session::SessionId.new(session.id.to_s).private_id
+
     logger.info("private_session: #{private_id}")
 
-
-    #db_session = Session.find_by_session_id(session.session_id)
-    #db_session.shib_session = shib_session
-    #db_session.save
+    db_session = Session.find_by_session_id(private_id)
+    db_session.shib_session = shib_session
+    db_session.save
 
     redirect_to :controller => 'main', :action => 'welcome'
   end
